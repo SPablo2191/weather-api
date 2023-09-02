6cad5f5c09cda1839e0d1bea6deb504b834f1a97
SPablo2191 <pablosandoval2191@gmail.com>
2023-09-01 23:15:36 -0300

    feat: get_weather petition added

--- src/app/routes/weather.py
+++ src/app/routes/weather.py
@@ -3,3 +3,7 @@ from fastapi.encoders import jsonable_encoder
 from src.app.models.weather import WeatherSchema
 
 router = APIRouter()
+
+@router.get("/api/weather", response_description="Weather data retrieved")
+async def get_weather(city : str, country : str):
+    return {"message": f"Hello {city} {country}"}
