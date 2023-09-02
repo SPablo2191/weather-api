f544442d6fd3e17838362e2eec6c1dba13040f9f
SPablo2191 <pablosandoval2191@gmail.com>
2023-09-02 10:16:47 -0300

    feat: enum Endpoint added

--- src/app/app.py
+++ src/app/app.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 from app.routes.weather import router as WeatherRouter
+from app.utils import EndpointName
 
 app = FastAPI()
 
-app.include_router(WeatherRouter, tags=["Weather"], prefix="/api/weather")
+app.include_router(WeatherRouter, tags=["Weather"], prefix=EndpointName.weather)
 
 
 @app.get("/", tags=["Root"])
--- src/app/utils.py
+++ src/app/utils.py
@@ -1,7 +1,11 @@
-from dotenv import load_doten
+from dotenv import load_dotenv
+from enum import Enum
 import os
 
 load_dotenv()
 
 weather_api_key = os.getenv('WEATHER_API_KEY')
 weather_api_url = os.getenv('WEATHER_API_URL')
+
+class EndpointName(str, Enum):
+    weather = "/api/weather"
