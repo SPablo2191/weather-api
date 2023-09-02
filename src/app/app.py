341039d61c039403142352537cc76715560643f7
SPablo2191 <pablosandoval2191@gmail.com>
2023-09-02 14:42:44 -0300

    fix: cors added

--- src/app/app.py
+++ src/app/app.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
 from app.routes.weather import router as WeatherRouter
 from app.utils import EndpointName
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.include_router(WeatherRouter, tags=["Weather"], prefix=EndpointName.weather)
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "https://weather-api-vgb9.onrender.com",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/", tags=["Root"])
