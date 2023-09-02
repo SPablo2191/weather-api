56dfabe575276be24d7a21323bec5d8e9304b0d5
SPablo2191 <pablosandoval2191@gmail.com>
2023-09-02 14:02:31 -0300

    fix: change default value

--- src/app/models/weather.py
+++ src/app/models/weather.py
@@ -4,13 +4,13 @@ from pydantic import BaseModel
 
 
 class WeatherSchema(BaseModel):
-    location_name: str 
-    temperature: str 
-    wind: str 
-    cloudiness: str 
-    pressure: str 
-    humidity: str
-    sunrise: str 
-    sunset: str
-    geo_coordinates: str 
-    requested_time: datetime 
+    location_name: str = ''
+    temperature: str = ''
+    wind: str = ''
+    cloudiness: str = ''
+    pressure: str = ''
+    humidity: str = ''
+    sunrise: str = ''
+    sunset: str = ''
+    geo_coordinates: str = ''
+    requested_time: datetime = datetime.now()
