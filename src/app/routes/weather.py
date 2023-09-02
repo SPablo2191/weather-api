52ec936a93e13ec4c341412e207e81d64af81c02
SPablo2191 <pablosandoval2191@gmail.com>
2023-09-02 10:17:00 -0300

    feat: get weather app directly

--- src/app/routes/weather.py
+++ src/app/routes/weather.py
@@ -1,9 +1,13 @@
 from fastapi import APIRouter, Body
 from fastapi.encoders import jsonable_encoder
 from app.models.weather import WeatherSchema
+from app.utils import weather_api_key,weather_api_url
+import requests
 
 router = APIRouter()
 
+
 @router.get("/", response_description="Weather data retrieved")
-async def get_weather(city : str, country : str):
-    return {"message": f"Hello {city} {country}"}
+async def get_weather(city: str, country: str):
+    response = requests.get(f'{weather_api_url}?q={city},{country}&appid={weather_api_key}')
+    return response.json()
