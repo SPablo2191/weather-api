8bdd266eb0b3fb35b7c0607860831767a31cce32
SPablo2191 <pablosandoval2191@gmail.com>
2023-09-01 22:31:14 -0300

    feat: WeatherSchema added

---
+++ src/app/schemas/weather.py
@@ -0,0 +1,17 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+class WeatherSchema(BaseModel):
+    location_name : str
+    temperature : str
+    wind : str
+    cloudiness : str
+    pressure : str
+    humidity : str
+    sunrise : str
+    sunset : str
+    geo_coordinates : str
+    requested_time : str
+
+
